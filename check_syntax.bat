30141f8c5c5c60186c31eb6c63cf09575e8ea54e
zhangweixsw <37978839+zhangweixsw@users.noreply.github.com>
2026-06-24 21:22:24 +0800

    Add check_syntax.bat

---
+++ check_syntax.bat
@@ -0,0 +1,4 @@
+"C:\Users\ASUS\AppData\Local\Programs\Python\Python313\python.exe" -m py_compile "E:\Windows 10 System\WeiLi syc\ALL IN ONE\Github Files\IntelliKnowGraphText 利用本地大模型处理pdf和图片\IntelliKnowGraphText\core\llm_client.py"
+if errorlevel 0 (
+    echo 语法正确
+)
